2b739744d62f1a6cd8de4c4b56c4c056879bcfe0
zswspike <2246777653@qq.com>
2020-01-23 11:20:44 +0800

    First Commit

---
+++ holiday/holiday/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  holiday
+//
+//  Created by ZSW on 2020/1/23.
+//  Copyright © 2020 ZSW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
